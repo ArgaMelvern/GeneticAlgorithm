e0efd57a87e7d2d725ecaf338861d7f6b7d00d73
Arga Melvern <82942149+ArgaMelvern@users.noreply.github.com>
2021-04-21 18:11:05 +0700

    Add files via upload

---
+++ TUBES.py
@@ -0,0 +1,11 @@
+def generate_population():
+    population = []
+    for i in range (10):
+        temp = []
+        for j in range (6):
+            temp.append(random.choice[0,1])
+            population.append(temp())
+    return population
+
+lol = generate_population[]
+print(lol)
